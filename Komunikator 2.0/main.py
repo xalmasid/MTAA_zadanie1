9f5ed488fc12d98d4b11b12bc819d06ba59727db
Daniel <xalmasid1@stuba.sk>
2022-02-27 19:44:06 +0100

    pridanie logovania

--- Komunikator 2.0/main.py
+++ Komunikator 2.0/main.py
@@ -1,6 +1,8 @@
 import socketserver
 import sipfullproxy as sfp
 import socket
+import logging
+import time
 
 
 def main():
@@ -8,7 +10,8 @@ def main():
     PORT = 5060
     HOST = "0.0.0.0"
     ip = socket.gethostbyname(socket.gethostname())
-    print(f"IP: {ip}:{PORT}")
+    print(ip + ":" + str(PORT))
+    logging.info('IP: ' + ip)
 
     sfp.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ip, PORT)
     sfp.topvia = "Via: SIP/2.0/UDP %s:%d" % (ip, PORT)
@@ -17,4 +20,7 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='mtaa_proxy.log', level=logging.INFO,
+                        datefmt='%H:%M:%S')
+    logging.info(time.strftime("\n\t======PROXY SERVER=======\n" + "%a, %d %b %Y %H:%M:%S", time.localtime()))
     main()
--- Komunikator 2.0/sipfullproxy.py
+++ Komunikator 2.0/sipfullproxy.py
@@ -414,13 +414,10 @@ class UDPHandler(socketserver.BaseRequestHandler):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy.log', level=logging.INFO,
-                        datefmt='%H:%M:%S')
-    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
     hostname = socket.gethostname()
     logging.info(hostname)
     ipaddress = socket.gethostbyname(socket.gethostname())
-    print(ipaddress)
+
     if ipaddress == "127.0.0.1":
         ipaddress = sys.argv[1]
     logging.info(ipaddress)
